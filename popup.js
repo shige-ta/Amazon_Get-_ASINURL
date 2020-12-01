d40734c85da5a54683d9100a199e7406fcc1fd1d
takahashi <takahashi@tk-MBA.local>
2020-12-02 03:41:33 +0900

    一回目でコピーできない不具合修正、たまにクリップボードにコピーするとエラーになる不具合を修正

--- popup.js
+++ popup.js
@@ -4,33 +4,25 @@ chrome.tabs.getSelected(null, function(tab){
     let startnum;
     console.log(url);
     let substr01;
-    while(1){
-        let startnum_dp = url.indexOf("/dp/");
-        let startnum_product = url.indexOf("/product/");
-        if(startnum_dp != -1){
-            startnum = startnum_dp;
-            substr01 = url.substr(startnum + 4);
-            break;
-        }
-        else if(startnum_product != -1){
-            startnum = startnum_product;
-            substr01 = url.substr(startnum + 9);
-            break;
-        }
+    let startnum_dp = url.indexOf("/dp/");
+    let startnum_product = url.indexOf("/product/");
+    if(startnum_dp != -1){
+        startnum = startnum_dp;
+        substr01 = url.substr(startnum + 4);
+    }
+    else if(startnum_product != -1){
+        startnum = startnum_product;
+        substr01 = url.substr(startnum + 9);
     }
     console.log(substr01);
-    let endnum;
-    while(1){
-        let endnum01 = substr01.indexOf("/");
-        let endnum02 = substr01.indexOf("?");
-        if(endnum01 != -1){
-            endnum = endnum01;
-            break;
-        }
-        else if(endnum02 != -1){
-            endnum = endnum02;
-            break;
-        }
+    let endnum = -1;
+    let endnum01 = substr01.indexOf("/");
+    let endnum02 = substr01.indexOf("?");
+    if(endnum01 != -1){
+        endnum = endnum01;
+    }
+    else if(endnum02 != -1){
+        endnum = endnum02;
     }
     console.log(endnum);
     if(endnum == -1){
@@ -39,9 +31,13 @@ chrome.tabs.getSelected(null, function(tab){
     let ASIN = substr01.substr(0, endnum);
     console.log(ASIN);
     clipboadURL = "https://www.amazon.co.jp/dp/" + ASIN;
-    if(navigator.clipboard){
-        navigator.clipboard.writeText(clipboadURL);
-    }
+    document.getElementById("test").value = clipboadURL;
+
+    document.oncopy = function(event) {
+        event.clipboardData.setData("text/plain", clipboadURL);
+        event.preventDefault();
+      };
+      document.execCommand("copy", false, null);
 })
 
 
